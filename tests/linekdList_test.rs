9e05b61a83b3f8024b53ebc2e6b25060b19b1601
PullolilEzequiel <ezequielkarottupullolil@gmail.com>
2024-01-14 09:26:05 -0300

    Added test for get_at method

--- tests/linekdList_test.rs
+++ tests/linekdList_test.rs
@@ -11,6 +11,24 @@ mod test {
     fn push_test() {
         let mut list = LinkedList::<u32>::new();
         list.push(10);
-        assert!(list.len() == 1);
+        assert_eq!(list.len(), 1);
+        assert_eq!(list.get_at(0), Ok(10));
+    }
+
+    #[test]
+    fn get_at_test() {
+        let mut list = LinkedList::<u32>::new();
+        for i in 0..10 {
+            list.push(i);
+        }
+
+        assert_eq!(list.len(), 10);
+
+        assert_eq!(list.get_at(5), Ok(4));
+        assert_eq!(list.get_at(9), Ok(0));
+        assert_eq!(
+            list.get_at(12),
+            Err("Nonexistent element in index 12".to_string())
+        );
     }
 }
